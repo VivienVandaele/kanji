86f7d6c1061643c63a169ce5cbbdbd616e837aaf
Vivien Vandaele <vivien.vandaele@grenoble-inp.org>
2019-06-30 16:00:39 +0000

    update

--- Connection.php
+++ Connection.php
@@ -4,7 +4,7 @@ class Connection{
 
     private function __construct(){
         try{
-            self::$bdd = new PDO('mysql:host=localhost;dbname=kanji', 'root', '');
+            self::$bdd = new PDO('mysql:host=localhost:8080;dbname=kanji', 'root', '');
         }
         catch(Exception $e){
             die('Erreur : '.$e->getMessage());
